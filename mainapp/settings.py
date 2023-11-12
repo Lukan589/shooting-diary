43c19f3e301d66f773e93ab646b23cf0c8b04dfd
Lukáš Andrés <andreslukas@email.cz>
2023-11-12 12:10:14 +0100

    Added Supabase Info

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Eg7Gen0Oj0AtM3nx',
+        'HOST': 'db.nybdebjzmewtmeeduivo.supabase.co',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
